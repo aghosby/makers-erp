7018af3c5ec5eae5b83bb914c7d248cad13ffd92
aghosby <etiboaghogho@gmail.com>
2026-02-10 18:45:06 +0000

    Fixed few errors

--- src/app/modules/hr/payroll/payroll-details/payroll-details.component.ts
+++ src/app/modules/hr/payroll/payroll-details/payroll-details.component.ts
@@ -64,7 +64,7 @@ export class PayrollDetailsComponent implements OnInit {
       isExisting: false
     }
     this.getPageData();
-    this.payrollCurrentStep = 1;
+    this.payrollCurrentStep = 0;
     this.payrollRunSteps = [
       {
         icon: 'calendarTime',
